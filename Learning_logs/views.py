90e5c0aaa261092699fe1feb4d2eb72aca51486b
samizzz67 <abiyafikre@gmail.com>
2026-03-06 19:43:58 +0300

    Add all files

---
+++ Learning_logs/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Topic
+# Create your views here.
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+def topics(request):
+    """show all topics."""
+    topics = Topic.objects.order_by('data_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
