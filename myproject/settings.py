dceb365291098c7557371f1839327bd44c3c8d69
samiZZZ67 <abiyafikre@gmail.com>
2026-04-07 10:23:58 +0300

    add files(user)

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #my apps
     'Learning_logs',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
