1abb9b817eea91e4b846edbced6781c6d635f102
samiZZZ67 <abiyafikre@gmail.com>
2026-04-09 21:25:01 +0300

    replace old templates with better and great performance,add views delete for topics and entry and also date for topic

--- Learning_logs/views.py
+++ Learning_logs/views.py
@@ -13,7 +13,7 @@ def index(request):
 @login_required
 def topics(request):
     """Show the current user's topics."""
-    topics = Topic.objects.order_by('data_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('-data_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 @login_required
@@ -34,14 +34,27 @@ def  new_topic(request):
     else:
         # POST data submitted; process data.
          form = TopicForm(data=request.POST)
+         
          if form.is_valid():
-            new_topic=form.save(commit=False)
-            new_topic.owner=request.user
-            new_topic.save()
-            return redirect('learning_logs:topics')
+            topic_name = form.cleaned_data.get('text')
+            if Topic.objects.filter(text__iexact=topic_name, owner=request.user).exists():
+                # Do something if it exists (e.g., show an error)
+                form.add_error('text', 'You already have a topic with this name.')
+            else:
+                new_topic=form.save(commit=False)
+                new_topic.owner=request.user
+                new_topic.save()
+                return redirect('learning_logs:topics')
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+@login_required
+def delete_topic(request,topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    check_topic_owner(request,topic)
+    topic.delete()
+    return  redirect("learning_logs:topics")
+
 @login_required
 def new_entry(request,topic_id):
     """Add a new entry for a particular topic."""
@@ -90,6 +103,18 @@ def edit_entry(request,entry_id):
             return redirect('learning_logs:topic',topic_id=topic.id)
     context= {'entry':entry, 'topic':topic,'form':form}
     return render (request, 'learning_logs/edit_entry.html',context)
-# def check_topic_owner(request,topic):
-#     if topic.owner != request.user:
-#         raise Http404
+@login_required
+def delete_entry(request,entry_id):
+    entry=get_object_or_404(Entry,id=entry_id)
+    topic=entry.topic
+    is_owner=(topic.owner==request.user)
+    if not is_owner:
+        return render (request,'learning_logs/not_owner.html',{
+            'topic':topic
+        })
+    else:
+        entry.delete()
+        return redirect('learning_logs:topic',topic_id=topic.id)
+def check_topic_owner(request,topic):
+    if topic.owner != request.user:
+        raise Http404
