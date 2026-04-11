6132dffad52c2b374a63688a3589dca527212eb2
samiZZZ67 <abiyafikre@gmail.com>
2026-04-11 20:51:40 +0300

    add admin with oraganized way

--- Learning_logs/admin.py
+++ Learning_logs/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Topic, Entry
-admin.site.register(Topic)
-admin.site.register(Entry)
+@admin.register(Topic)
+class Topicadmin(admin.ModelAdmin):
+    list_display = ['text', 'data_added']
 
-# Register your models hes
+    list_filter = ['data_added']
+    search_fields = ['text']
+    date_hierarchy='data_added'
+    show_facets = admin.ShowFacets.ALWAYS
+admin.site.register(Entry)
